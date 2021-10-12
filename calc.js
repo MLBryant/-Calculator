db55d1abcf570443d593a5e837fd837802b8d00c
Mason Bryant <mlbryant6@gmail.com>
2021-10-12 14:40:05 -0500

    Added Addition Feature

--- calc.js
+++ calc.js
@@ -10,9 +10,15 @@ const reader = readline.createInterface({
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
-	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
+	mathSymbol = tokens[1];
+	num1 = Number(tokens[0]);
 	num2 = Number(tokens[2]);
+
+	if (mathSymbol == '+') {
+		console.log(num1 + num2);
+	}
+
+	console.log('mathSymbol ', mathSymbol);
 
 
 	// This line closes the connection to the command line interface.
